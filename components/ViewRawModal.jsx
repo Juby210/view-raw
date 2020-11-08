2e507398efe8699d957959a2f3aaa3d35ae2ad89
Kyza <40003440+KyzaGitHub@users.noreply.github.com>
2020-11-07 21:10:52 -0500

    Update ViewRawModal.jsx

--- components/ViewRawModal.jsx
+++ components/ViewRawModal.jsx
@@ -98,6 +98,4 @@ class ViewRawModal extends React.PureComponent {
 }
 
 // Might as well.
-module.exports = KLibrary?.Tools?.ReactTools?.WrapBoundary
-	? KLibrary.Tools.ReactTools.WrapBoundary(ViewRawModal)
-	: ViewRawModal;
+module.exports = KLibrary?.Tools?.ReactTools?.WrapBoundary(ViewRawModal) || ViewRawModal;
