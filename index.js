9d5848da504914ad6bf516a834cd2c883ef86700
Juby210 <31005896+Juby210@users.noreply.github.com>
2022-01-16 16:05:18 +0100

    simplify logic

--- index.js
+++ index.js
@@ -4,19 +4,11 @@ const { findInReactTree } = require("powercord/util");
 const { getModule, getModuleByDisplayName, React } = require("powercord/webpack");
 const { inject, uninject } = require("powercord/injector");
 
-const { clipboard } = require('electron');
-
 const Settings = require("./components/Settings");
 const ViewRawButton = require("./components/ViewRawButton");
 const Modal = require("./components/ViewRawModal");
 
 module.exports = class ViewRaw extends Plugin {
-	constructor() {
-		super();
-		this.injectMessageContextMenu = this.injectMessageContextMenu.bind(this);
-		this.injectContextMenus = {};
-	}
-
 	startPlugin() {
 		powercord.api.settings.registerSettings(this.entityID, {
 			category: this.entityID,
@@ -29,8 +21,7 @@ module.exports = class ViewRaw extends Plugin {
 		});
 		this.loadStylesheet("style.css");
 
-		this.injectOpenContextMenuLazy();
-		this.addButtons();
+		this.addButtons()
 	}
 
 	pluginWillUnload() {
@@ -39,14 +30,13 @@ module.exports = class ViewRaw extends Plugin {
 		document
 			.querySelectorAll(".view-raw-button")
 			.forEach((e) => (e.style.display = "none"));
-
-		uninject("view-raw-context-lazy-menu");
 	}
 
 	async addButtons(repatch, unpatch) {
 		if (repatch) {
-			uninject("view-raw-toolbar");
-			uninject("view-raw-contextmenu");
+			uninject('view-raw-toolbar')
+			uninject('view-raw-contextmenu')
+			uninject('view-raw-context-lazy-menu')
 		}
 		if (unpatch) return;
 
@@ -71,20 +61,10 @@ module.exports = class ViewRaw extends Plugin {
 
 		if (!this.settings.get("contextMenu", true)) return;
 
-		this.injectContextMenus.MessageContextMenu = this.injectMessageContextMenu;
-	}
-
-	injectMessageContextMenu() {
-		const { MenuGroup, MenuItem } = getModule(["MenuGroup", "MenuItem"], false);
-		const MessageContextMenu = getModule(
-			(m) => m?.default?.displayName === "MessageContextMenu", false
-		);
-		inject(
-			"view-raw-contextmenu",
-			MessageContextMenu,
-			"default",
-			(args, res) => {
-				if (!args[0]?.message || !res?.props?.children) return res;
+		this.lazyPatchContextMenu('MessageContextMenu', MessageContextMenu => {
+			const { MenuGroup, MenuItem } = getModule(['MenuGroup', 'MenuItem'], false)
+			inject('view-raw-contextmenu', MessageContextMenu, 'default', (args, res) => {
+				if (!args[0]?.message || !res?.props?.children) return res
 
 				res.props.children.splice(
 					4,
@@ -96,54 +76,50 @@ module.exports = class ViewRaw extends Plugin {
 							action: () =>
 								open(() =>
 									React.createElement(Modal, {
-										allRawData: this.settings.get("allRawData"),
+										allRawData: this.settings.get('allRawData'),
 										message: args[0].message,
 									})
 								),
-							id: "view-raw",
-							label: "View Raw",
+							id: 'view-raw',
+							label: 'View Raw',
 						}),
 						React.createElement(MenuItem, {
-							action: () => clipboard.copy(args[0].message.content),
+							action: () => DiscordNative.clipboard.copy(args[0].message.content),
 							disabled: !args[0].message.content,
-							id: "copy-raw",
-							label: "Copy Raw",
+							id: 'copy-raw',
+							label: 'Copy Raw',
 						})
 					)
-				);
-				return res;
-			}
-		);
-		MessageContextMenu.default.displayName = "MessageContextMenu";
+				)
+				return res
+			})
+			MessageContextMenu.default.displayName = 'MessageContextMenu'
+		})
 	}
 
-	injectOpenContextMenuLazy () {
-		const module = getModule([ 'openContextMenuLazy' ], false);
+	async lazyPatchContextMenu(displayName, patch) {
+		const filter = m => m.default && m.default.displayName === displayName
+		const m = getModule(filter, false)
+		if (m) patch(m)
+		else {
+			const module = getModule([ 'openContextMenuLazy' ], false)
+			inject('view-raw-context-lazy-menu', module, 'openContextMenuLazy', args => {
+				const lazyRender = args[1]
+				args[1] = async () => {
+					const render = await lazyRender(args[0])
 
-		inject('view-raw-context-lazy-menu', module, 'openContextMenuLazy', ([ event, lazyRender, params ]) => {
-			const warpLazyRender = async () => {
-				const render = await lazyRender(event);
-
-				return (config) => {
-					const menu = render(config);
-					const CMName = menu?.type?.displayName;
-
-					if (CMName) {
-						const moduleByDisplayName = getModuleByDisplayName(CMName, false);
-
-						if (CMName in this.injectContextMenus) {
-							this.injectContextMenus[CMName]();
-							delete this.injectContextMenus[CMName];
+					return (config) => {
+						const menu = render(config)
+						if (menu?.type?.displayName === displayName && patch) {
+							uninject('view-raw-context-lazy-menu')
+							patch(getModule(filter, false))
+							patch = false
 						}
-						if (moduleByDisplayName !== null) {
-							menu.type = moduleByDisplayName;
-						}
+						return menu
 					}
-					return menu;
-				};
-			};
-
-			return [ event, warpLazyRender, params ];
-		}, true);
+				}
+				return args
+			}, true)
+		}
 	}
 };
